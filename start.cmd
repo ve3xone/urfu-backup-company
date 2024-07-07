71f7585b9fbfb734fa0855762e794bcec88b9cfa
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-07-07 12:10:57 +0500

    Убрал все скрипты из истории

--- start.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Users\ve3xone\.jdks\corretto-1.8.0_412
-set ExecPath=%cd%
-title %ExecPath%
-cd %ExecPath%
-:run
-"%ExecPath%\venv\Scripts\activate.bat"&"%ExecPath%\venv\Scripts\python.exe" main.py&pause
